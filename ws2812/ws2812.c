95d7c00d7d716be5a87643c2993ddb4f8f83aedc
tifoss <me@tifoss.eu>
2024-04-14 11:14:39 +0200

    add ws2812 lib from pico-examples
    Signed-off-by: tifoss <me@tifoss.eu>

--- ws2812/ws2812.c
+++ ws2812/ws2812.c
@@ -1,14 +1,26 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
-#include "ws2812/generated/ws2812.pio.h"
+#include "ws2812.pio.h"
 
-#define IS_RGBW false
-#define NUM_PIXELS 40
+#define IS_RGBW true
+#define NUM_PIXELS 150
+
+#ifdef PICO_DEFAULT_WS2812_PIN
+#define WS2812_PIN PICO_DEFAULT_WS2812_PIN
+#else
+// default to pin 2 if the board doesn't have a default WS2812 pin defined
 #define WS2812_PIN 2
+#endif
 
 static inline void put_pixel(uint32_t pixel_grb) {
     pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
@@ -58,10 +70,23 @@ void pattern_greys(uint len, uint t) {
     }
 }
 
-int main()
-{
-    stdio_init_all();
+typedef void (*pattern)(uint len, uint t);
+const struct {
+    pattern pat;
+    const char *name;
+} pattern_table[] = {
+        {pattern_snakes,  "Snakes!"},
+        {pattern_random,  "Random data"},
+        {pattern_sparkle, "Sparkles"},
+        {pattern_greys,   "Greys"},
+};
 
+int main() {
+    //set_sys_clock_48();
+    stdio_init_all();
+    printf("WS2812 Smoke Test, using pin %d", WS2812_PIN);
+
+    // todo get free sm
     PIO pio = pio0;
     int sm = 0;
     uint offset = pio_add_program(pio, &ws2812_program);
@@ -70,36 +95,14 @@ int main()
 
     int t = 0;
     while (1) {
-        puts("snakes");
-        for(int i=0; i<1000; i++) {
-            pattern_snakes(NUM_PIXELS, i);
+        int pat = rand() % count_of(pattern_table);
+        int dir = (rand() >> 30) & 1 ? 1 : -1;
+        puts(pattern_table[pat].name);
+        puts(dir == 1 ? "(forward)" : "(backward)");
+        for (int i = 0; i < 1000; ++i) {
+            pattern_table[pat].pat(NUM_PIXELS, t);
             sleep_ms(10);
-        }
-
-        sleep_ms(500);
-
-        puts("random");
-        for(int i=0; i<1000; i++) {
-            pattern_random(NUM_PIXELS, i);
-            sleep_ms(10);
-        }
-
-        sleep_ms(500);
-
-        puts("sparkle");
-        for(int i=0; i<1000; i++) {
-            pattern_sparkle(NUM_PIXELS, i);
-            sleep_ms(10);
-        }
-
-        sleep_ms(500);
-
-        puts("greys");
-        for(int i=0; i<1000; i++) {
-            pattern_greys(NUM_PIXELS, i);
-            sleep_ms(10);
+            t += dir;
         }
     }
-
-    return 0;
 }
